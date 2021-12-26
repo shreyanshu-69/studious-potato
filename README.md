488ccf95a842aed4928f8221bb87092f9ab92a05
Shreyanshu <82714857+shreyanshu-69@users.noreply.github.com>
2021-12-26 10:17:23 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+### A Little Form Made With Python 
+## IDk Why I Make These Kinds Of Stuffs LoL
---
+++ main.py
@@ -0,0 +1,10 @@
+print('What Is Your Name?')
+name = input('') #inputs the name 
+print (name, 'Welcome To Form Fill Up')
+print ('Whats Your Age?')
+age = input('') #inputs age 
+print ('Please Enter Your Github Profile Below :')
+profile = input ('') #inputs github profile
+print ('Whats Your Major Goal For This Year')
+goal = input ('') #inputs goal
+print ('Thanks for Filling This Form, More Updates On This Form Is rolling Out Soon!')
